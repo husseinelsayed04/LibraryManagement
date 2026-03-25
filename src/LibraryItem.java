1269111eae27a6c317996555624480167cf415e7
Hussein Elsayed <hussainelsayed04@gmail.com>
2026-03-25 21:27:50 +0200

    Add abstract class LibraryItem with attributes and constructor

---
+++ src/LibraryItem.java
@@ -0,0 +1,35 @@
+public abstract class LibraryItem {
+    private String title;
+    private int publication;
+    private int id;
+    private int numOfPages;
+    private boolean isAvailable;
+
+    public LibraryItem(String title, int publication, int id, int numOfPages, boolean isAvailable) {
+        this.title = title;
+        this.publication = publication;
+        this.id = id;
+        this.numOfPages = numOfPages;
+        this.isAvailable = isAvailable;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getPublication() {
+        return publication;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getNumOfPages() {
+        return numOfPages;
+    }
+
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+}
