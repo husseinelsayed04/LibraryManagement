20cae957689c2ef03fdfea1e5408ed51fc6e9596
Hussein Elsayed <hussainelsayed04@gmail.com>
2026-03-25 22:10:05 +0200

    Add Journal class extending LibrartItem with researchField attribute

---
+++ src/Journal.java
@@ -0,0 +1,8 @@
+public class Journal extends LibraryItem {
+    private String researchField;
+
+    public Journal(String title, int publication, int id, int pages, boolean isAvailable, String researchField) {
+        super(title, publication, id, pages, isAvailable);
+        this.researchField = researchField;
+    }
+}
